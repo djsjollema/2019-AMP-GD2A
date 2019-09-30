612eab3249382a21093322245f8f4bc98ae3a1bc
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-09-30 11:39:22 +0200

    06_03

--- html/06intersectingLines/script.js
+++ html/06intersectingLines/script.js
@@ -8,14 +8,27 @@ canvas.width = width;
 canvas.height = height;
 
 let A = new Point(new Vector2d(200,200),15,"red","A",true);
-let B = new Point(new Vector2d(500,200),15,"blue","B",true);
+let B = new Point(new Vector2d(700,400),15,"blue","B",true);
+let C = new Point(new Vector2d(800,100),15,"green","C",true);
+let D = new Point(new Vector2d(300,500),15,"yellow","D",true);
+let S = new Point(new Vector2d(500,400),10,"white","S",false)
 let l = new LinearFunction(1,1);
+let m = new LinearFunction(-1,100);
 
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
+  l.defineLineWithTwoPoints(A,B);
+  l.draw(context);
+
+  m.defineLineWithTwoPoints(C,D);
+  m.draw(context);
+
   A.draw(context);
   B.draw(context);
+  C.draw(context);
+  D.draw(context);
+  S.draw(context);
 }
 
 animate();
