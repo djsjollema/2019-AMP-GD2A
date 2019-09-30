cd597d6d0b6a1333a6ad908e92683df788a5755b
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-09-30 10:59:05 +0200

    06_01

---
+++ html/06intersectingLines/script.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+let A = new Point(new Vector2d(200,200),15,"red","A",true);
+let B = new Point(new Vector2d(500,200),15,"blue","B",true);
+
+function animate(){
+  requestAnimationFrame(animate);
+  context.clearRect(0,0,width,height);
+  A.draw(context);
+  B.draw(context);
+}
+
+animate();
