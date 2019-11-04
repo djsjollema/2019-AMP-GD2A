9f00e096f811e780e2cd225fe79fe027958ae94b
djsjollema <j.sjollema@ma-web.nl>
2019-11-04 15:05:58 +0100

    car --wheels

--- html/12myCar/script.js
+++ html/12myCar/script.js
@@ -12,17 +12,27 @@ car.src = 'car.png';
 car.position = new Vector2d(0,0);
 car.velocity = new Vector2d(7,0);
 
-car.position.dy = height - car.height
+let frontwheel = new Image();
+frontwheel.src = "wheel.png";
+frontwheel.position = new Vector2d(0,0);
+
+
+car.addEventListener('load',()=>{
+  car.position.dy = height - car.height;
+  animate();
+})
+
 
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
   car.position.add(car.velocity)
   context.drawImage(car, car.position.dx,car.position.dy);
+
+  frontwheel.position.sumVector(car.position,new Vector2d(132,120));
+  context.drawImage(frontwheel, frontwheel.position.dx,frontwheel.position.dy);
   clamp()
 }
-
-animate();
 
 function clamp(){
   if(car.position.dx > width){
