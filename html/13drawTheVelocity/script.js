99b4ae82dcac95d62b7811574d27f99da93be0e4
djsjollema <j.sjollema@ma-web.nl>
2019-11-19 11:39:28 +0100

    13 --DPoint --update

--- html/13drawTheVelocity/script.js
+++ html/13drawTheVelocity/script.js
@@ -7,11 +7,12 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let point = new DPoint(new Vector2d(200,200),new Vector2d(3,4),new Vector2d(0,0),20,"yellow","point")
+let point = new DPoint(new Vector2d(200,200),new Vector2d(12,14),new Vector2d(0,0),20,"yellow","point")
 
 function animate(){
-  //requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
+  point.update();
   point.draw(context)
 }
 
