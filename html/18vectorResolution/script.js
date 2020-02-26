483b4adb50ce98f949e723bc3eb1f97a03888be7
djsjollema <j.sjollema@ma-web.nl>
2020-02-26 16:07:04 +0100

    18 vector resolution --pointc

--- html/18vectorResolution/script.js
+++ html/18vectorResolution/script.js
@@ -9,6 +9,10 @@ canvas.height = height;
 
 let A = new Point(new Vector2d(200,300),20,"red","A",true);
 let B = new Point(new Vector2d(500,400),20,"blue","B",true);
+let C = new Point(new Vector2d(250,500),20,"yellow","C",true);
+
+let vector = new Vector2d(1,1);
+let rad = new Vector2d(1,1);
 
 let grid = new Grid();
 
@@ -19,6 +23,15 @@ function animate(){
   grid.draw(context);
   A.draw(context);
   B.draw(context);
+  C.draw(context);
+  vector.dx = B.position.dx - A.position.dx;
+  vector.dy =B.position.dy - A.position.dy;
+
+  rad.dx = C.position.dx - A.position.dx;
+  rad.dy = C.position.dy - A.position.dy;
+
+  vector.draw(context,A.position,1);
+  rad.draw(context,A.position,1);
 }
 
 animate();
